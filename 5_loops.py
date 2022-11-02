6f97c12a96727fca802e40a063c06ff3dab4acb4
Nikita Joan Abuya <112301@STD.STRATHMORE.LOCAL>
2022-11-02 20:08:05 +0300

    class2

---
+++ 5_loops.py
@@ -0,0 +1,10 @@
+# Python implements loops using the 'for' keyword
+# Accompanied by some additional logic
+my_fruits = ["Mango","Orange", "Peach", "Apple"]
+for fruit in my_fruits:
+    print(fruit)
+print(f"The list{my_fruits} has {len (my_fruits)} items")
+
+my_name = "Jonathan"
+for i in my_name:
+    print(i)
