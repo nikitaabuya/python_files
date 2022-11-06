e1292124a738fa4e84a89ed5bd0cf173387d674b
nikitaabuya <kittaabuya@gmail.com>
2022-11-06 14:55:38 +0300

    class3

--- 6_lists.py
+++ 6_lists.py
@@ -1,25 +1,36 @@
-#declare and initialize a list
+# Declare and initialize a list
 fruits = ["Orange", "Peach", "Banana", "Mango", "Plum"]
 print(fruits)
 
-#loop through entire list of fruits
+# Loop through entire list of fruits
 for fruit in fruits:
     print(fruit)
 
-# retrieve list items
+# Retrieve list items
 print(f"The fruit at position 1: {fruits[0]}")
-print(f"The fruit at index 1: {fruits[1]}")
+print(f"The fruit at end of the list: {fruits[-1]}")
 
-#check length of the list
+# Check length of the list
 print(f"The fruit basket currently has {len(fruits)} fruits")
 
-# add something to the list
+# Add something to the list
 fruits.append("Lemon")
 print(fruits)
+# Check length of the list
+print(f"The fruit basket currently has {len(fruits)} fruits")
 
-#check length of the list
-print(f"the fruit basket currently has {len(fruits)} fruits ")
-
-#add a list item to a specific position in the list
+# Add a list item to a specific position in the list
 fruits.insert(2, "Apple")
+print(fruits)
+
+# Remove specific item from the list
+fruits.remove("Lemon")
+print(fruits)
+
+# Remove item from a specified index on list
+fruits.pop(0)
+print(fruits)
+
+# Remove item from the end of list
+fruits.pop()
 print(fruits)
--- 7_sets.py
+++ 7_sets.py
@@ -1,20 +1,20 @@
-#create and initialize a set
-my_cities = {"Kampala", "Nairobi", "Arusha", "Mombasa", "Nairobi"}
+# Create and initialize a set
+ea_cities = {"Kampala", "Nairobi", "Arusha", "Mombasa", "Nairobi"}
 
-print(my_cities)
-print (f"There are {len(my_cities)} cities in the set")
+print(ea_cities)
 
-print(type(my_cities))
+print(f"There are {len(ea_cities)} cities in the set")
 
-my_cities.add("Kigali")
-print(my_cities)
+print(type(ea_cities))
+
+ea_cities.add("Kigali")
+print(ea_cities)
 
 west_african_cities = {"Lagos", "Accra"}
 south_african_cities = {"Cape Town", "Durban"}
 
 master_city_list = {"New York"}
-master_city_list.update(ea_cities)
-print(master_city_list.update(ea_cities))
 
-master_city_list.update(west_african_cities)
-master_city_list.update(south_african_cities)
+master_city_list.update(ea_cities)
+
+print(master_city_list)
